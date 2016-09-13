d4912e0829d4d38c61ac87f9eab7355ed729aff1
Coyla <work@MacBook-Pro-de-Juan.local>
2016-09-13 04:19:08 +0200

    Jeu Stratégie

---
+++ Time-Agotchi/MessageAdvertBrasDeFer.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Time_Agotchi
+{
+    partial class MessageAdvertBrasDeFer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_continuer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Quartz MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selectionne un adversaire";
+            // 
+            // btn_continuer
+            // 
+            this.btn_continuer.Font = new System.Drawing.Font("Quartz MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_continuer.ForeColor = System.Drawing.Color.SpringGreen;
+            this.btn_continuer.Location = new System.Drawing.Point(166, 79);
+            this.btn_continuer.Name = "btn_continuer";
+            this.btn_continuer.Size = new System.Drawing.Size(106, 35);
+            this.btn_continuer.TabIndex = 1;
+            this.btn_continuer.Text = "Continuer";
+            this.btn_continuer.UseVisualStyleBackColor = true;
+            this.btn_continuer.Click += new System.EventHandler(this.btn_continuer_Click);
+            // 
+            // MessageAdvertBrasDeFer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(292, 126);
+            this.Controls.Add(this.btn_continuer);
+            this.Controls.Add(this.label1);
+            this.Name = "MessageAdvertBrasDeFer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message Advert";
+            this.Load += new System.EventHandler(this.MessageAdvertBrasDeFer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_continuer;
+    }
+}
---
+++ Time-Agotchi/MessageAdvertBrasDeFer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Time_Agotchi
+{
+    public partial class MessageAdvertBrasDeFer : Form
+    {
+        public MessageAdvertBrasDeFer()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageAdvertBrasDeFer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_continuer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
